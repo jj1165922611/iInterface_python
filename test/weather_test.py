2b817d0063209ba710f38718a845b1b358f16e50
ytq <ytq12875@163.com>
2020-06-19 12:44:43 +0800

    submit

--- test/weather_test.py
+++ test/weather_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from library.httpclient import HttpClient
 
 
@@ -7,26 +9,12 @@ class TestWeather:
     def setup(self):
         """"""
         self.host = 'http://www.weather.com.cn'
-        self.ep_path = '/data/cityinfo'
+        self.api = '/data/cityinfo'
         self.client = HttpClient()
 
-    def test_1(self):
-        city_code = '101280601'
-        exp_city = '深圳'
-        self._test(city_code, exp_city)
-
-    def test_2(self):
-        city_code = '101010100'
-        exp_city = '北京'
-        self._test(city_code, exp_city)
-
-    def test_3(self):
-        city_code = '101020100'
-        exp_city = '上海'
-        self._test(city_code, exp_city)
-
-    def _test(self, city_code, exp_city):
-        url = f'{self.host}{self.ep_path}/{city_code}.html'
+    @pytest.mark.parametrize('city_code, exp_city',[("101280601","深圳1"),("101010100","北京"),("101020100","上海")])
+    def test_weather(self, city_code, exp_city):
+        url = f'{self.host}{self.api}/{city_code}.html'
         response = self.client.Get(url=url)
         act_city = response.json()['weatherinfo']['city']
         print(f'Expect city = {exp_city}, while actual city = {act_city}')
